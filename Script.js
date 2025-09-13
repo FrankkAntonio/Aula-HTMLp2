8e6aa6b23600d608eae9992f9046dd491c045f11
Frankk Antonio <frankkantonio69@gmail.com>
2025-09-13 12:16:42 -0300

    nova versão

---
+++ Script.js
@@ -0,0 +1,21 @@
+function triangulo(lado1, lado2, lado3) {
+    return lado1 + lado2 + lado3
+}
+console.log(triangulo(20, 30, 50))
+
+
+let nomea = nome => {
+console.log("Teste de funções")   
+console.log("Estou ultilizando o arrow function")    
+    
+console.log(nome);
+}
+
+function exibirNome(nome){
+
+    return nome;
+
+}
+
+
+
